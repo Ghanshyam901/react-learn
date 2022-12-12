322370823f18180a4a9f2b3310af59b87a2a72f1
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2022-12-13 01:10:55 +0530

    first

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import Todo from "./components/Todo";
+
+
+function App() {
+  return (
+    <div>
+      <h1>tOdO</h1>
+      <Todo text="todo"  />
+      <Todo text="todo 1" />
+      <Todo text="todo 2"/>
+   
+    </div>
+  );
+}
+
+export default App;
